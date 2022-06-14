1e49c29d77c3f4509da0a8a3183eddc6d0dfbd3e
Moe-Hassan-123 <mdhn6832@gmail.com>
2022-06-14 04:35:19 +0200

    Inital setup

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_session import Session
+from student import Student
+from myFuncs import login_required
 
 app = Flask(__name__)
 
-@app.route("/")
+
+# configuring Sessions
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
-@app.route("/register",methods=["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
+@login_required
 def register():
-    return render_template("register.html")
+    """
+    Register Function That allows the users to save thier data in The database
+    """
+    if request.method == "GET":
+        return render_template("register.html")
+
+    name = request.form.get()
+
 
 app.run(debug=True)
