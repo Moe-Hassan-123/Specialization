0fe64a1a085b7b8bc4abb462d5d2aa1993d76853
Moe-Hassan-123 <mdhn6832@gmail.com>
2022-06-15 03:52:40 +0200

    added a class that interacts with sqlite database

--- student.py
+++ student.py
@@ -7,25 +7,35 @@ class Student:
     A class that stores students in a database and allow mutiple function on them.
     """
 
-    db = sqlite3.connect("students.db")
-
     def __init__(
         self,
         first: str,
         last: str,
         national_id: bytes,
         password: bytes,
+        grade: int,
         specialization: str,
     ):
         """
-        It Creates the student in the database and stores his hashed national_id for other functions
+        It Creates the student in the database.
         """
-        Student.db.execute(
+        self.db = sqlite3.connect("students.db")
+        self.cur = self.db.cursor()
+        self.name = f"{first} {last}"
+        self.cur.execute("SELECT national_id FROM students")
+        for hashed_id in self.cur.fetchall():
+            if check_password_hash(hashed_id[0],national_id) == True:
+                print("User Exists Already")
+                self.cur.execute("SELECT ")
+                return
+            
+        self.cur.execute(
             "INSERT INTO students(name,password,national_id,specialization) Values(?,?,?,?)",
-            (f"{first} {last}", password, national_id, specialization),
+            (self.name,password, generate_password_hash(national_id), specialization),
         )
-        Student.db.commit()
-        self._id = national_id
+        self._id = self.cur.lastrowid
+        print(self.cur.lastrowid)
+        self.db.commit()
 
     @property
     def password(self):
@@ -43,16 +53,17 @@ class Student:
 
 
 def test_student():
-    values = [
-        "mohamed",
-        "hassan",
-        generate_password_hash("hi"),
-        generate_password_hash("hello"),
-        "scientific",
-    ]
-    student = Student(*values)
+    values = {
+        "first" : "mohamed",
+        "last" : "hassan",
+        "password" : generate_password_hash("12345"),
+        "national_id" : "3051232156488",
+        "grade" : 11,
+        "specialization" : "scientific",
+    }
+    student = Student(**values)
     return
 
 
 if __name__ == "__main__":
-    print(test_student())
+    test_student()
