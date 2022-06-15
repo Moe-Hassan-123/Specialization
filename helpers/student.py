cfc8b716d5977af124938c9035cf064d10267a27
Moe-Hassan-123 <mdhn6832@gmail.com>
2022-06-15 07:06:40 +0200

    added a setter method for national_id, name, password

--- helpers/student.py
+++ helpers/student.py
@@ -1,6 +1,7 @@
 import sqlite3
+from re import fullmatch
+from regex import P
 from werkzeug.security import check_password_hash, generate_password_hash
-
 
 class Student:
     """
@@ -8,53 +9,34 @@ class Student:
     """
 
     db = sqlite3.connect("students.db")
+    cur = db.cursor()
 
     def __init__(
         self,
-        first: str,
-        last: str,
-        national_id: bytes,
-        password: bytes,
+        name : str,
+        national_id: str,
+        password: str,
         grade: int,
         specialization: str,
     ):
         """
         It Creates the student in the database.
         """
-        self.db = sqlite3.connect("students.db")
-        self.cur = self.db.cursor()
-        self.name = f"{first} {last}"
-        self.cur.execute("SELECT national_id FROM students")
-        for hashed_id in self.cur.fetchall():
-            if check_password_hash(hashed_id[0],national_id) == True:
-                print("User Exists Already")
-                self.cur.execute("SELECT ")
-                return
-            
-        self.cur.execute(
-            "INSERT INTO students(name,password,national_id,specialization) Values(?,?,?,?)",
-            (self.name,password, generate_password_hash(national_id), specialization),
+        self.process_national_id(national_id)
+        self.process_password(password)
+        if grade not in [11,12]:
+            raise ValueError("Grade must be 11 or 12") 
+        Student.cur.execute(
+            "INSERT INTO students(name,password,national_id,grade,specialization) Values(?,?,?,?,?)",
+            (name,generate_password_hash(password), generate_password_hash(national_id), grade, specialization),
         )
         self._id = self.cur.lastrowid
-        print(self.cur.lastrowid)
-        self.db.commit()
-        specialization: str,
-    ):
-        """
-        It Creates the student in the database and stores his hashed national_id for other functions
-        """
-        Student.db.execute(
-            "INSERT INTO students(name,password,national_id,specialization) Values(?,?,?,?)",
-            (f"{first} {last}", password, national_id, specialization),
-        )
         Student.db.commit()
-        self._id = national_id
 
     @property
     def password(self):
-        curs = Student.db.cursor()
-        curs.execute("SELECT password FROM students WHERE national_id = ?",[self._id])
-        return curs.fetchone()[0]
+        Student.curs.execute("SELECT password FROM students WHERE national_id = ?",[self._id])
+        return Student.curs.fetchone()[0]
 
     @password.setter
     def password(cls):
@@ -64,27 +46,36 @@ class Student:
     def id(self):
         return self._id
 
+    @property
+    def national_id():
+        ...
+    
+    @national_id.setter
+    def national_id(self,national_id):
+        # check if it follows the format
+        if fullmatch(r"(2|3)\d{4}([1-9]|[12][0-9]|3[01])\d{6}[1-9]",national_id) is None:
+            raise ValueError("Incorrect National Id")
+        self._national_id = generate_password_hash(national_id)
+    @property
+    def password():
+        ...
 
-def test_student():
-    values = {
-        "first" : "mohamed",
-        "last" : "hassan",
-        "password" : generate_password_hash("12345"),
-        "national_id" : "3051232156488",
-        "grade" : 11,
-        "specialization" : "scientific",
-    }
-    student = Student(**values)
-    values = [
-        "mohamed",
-        "hassan",
-        generate_password_hash("hi"),
-        generate_password_hash("hello"),
-        "scientific",
-    ]
-    student = Student(*values)
-    return
+    @password.setter
+    def password(self,password):
+        # got this pattern from RockOnGom's comment on stackoverflow(https://stackoverflow.com/a/5142164)
+        pattern = r"(?=.*([A-Z]){1,})(?=.*[!@#$&*]{1,})(?=.*[0-9]{1,})(?=.*[a-z]{1,}).{8,100}"
+        if fullmatch(pattern,password) is None:
+            raise ValueError("Weak Password")
+        self._password = generate_password_hash(password)
 
-
-if __name__ == "__main__":
-    test_student()
+    @property
+    def name():
+        ...
+    @name.setter
+    def name(self,name):
+        # this pattern matches 3 arabic words with a single space inbetween
+        pattern = r"(?:[\u0621-\u064A]+ ){2}[\u0621-\u064A]+"
+        if fullmatch(pattern,name) is None:
+            raise ValueError("Should Provide a Full Name in Arabic\n Example: \"محمد حسن محمد\"")
+        self.name = name
+            
