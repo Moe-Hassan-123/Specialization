487b842fdc5b025179507e81645cb7b4eefd29f1
Moe-Hassan-123 <mdhn6832@gmail.com>
2022-06-24 15:45:46 +0200

    added Dynamic specialization choices depending on the selected grade

---
+++ static/choices.js
@@ -0,0 +1,17 @@
+/* This should change specialization options depending on the academic year the student claims to be in */
+
+document.addEventListener("DOMContentLoaded",function() {
+    grade = document.getElementById("grade")
+    grade.addEventListener("change",function(){
+        grade_value = grade.options[grade.selectedIndex].value
+        special = document.getElementById("specializations")
+        if (grade_value == "second")
+        {
+            special.innerHTML = '<option selected>اختر التخصص</option><option value="second">علمي</option><option value="third">ادبي</option>'
+        }
+        else if (grade_value == "third")
+        {
+            special.innerHTML = '<option selected>اختر التخصص</option><option value="second">علم رياضة</option><option value="third">علم علوم</option><option value="third">ادبي</option>' 
+        }
+    })
+})
